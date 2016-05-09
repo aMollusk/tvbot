e7e32d73c88268a3515bcb41074dad13f98b43c6
Kieran <kieran.mcdonald@pacificmags.com.au>
2016-05-09 13:48:40 +1000

    refactors and response method fixes

--- bin/bot.js
+++ bin/bot.js
@@ -4,7 +4,7 @@ var request = require('request');
 
 var Lol = require('../lib/app.js');
 
-var token = 'xoxb-40314032773-dDXuKYROzSpKKVYvaIIJTR52';
+var token = 'xoxb-40314032773-H4dHpIi5s2R4M5Qx2vvEBwgq';
 var name = 'tvbot';
 
 var lol = new Lol({
@@ -15,50 +15,19 @@ var lol = new Lol({
 lol.run();
 
 lol.on('start', function(){
-    lol.postMessageToChannel('general', 'yay');
-
+    lol.postMessageToChannel('general', 'Hey! Tvbot is action-town');
 })
 
 lol.on('message', function(data){
 
-
   if (data.type == 'message') {
     // console.log(this._mentionTvbot(data.text))
-    var instruction = this._mentionTvbot(data.text)
-    if(instruction.active === true && instruction.command != 'none'){
-      var tvObject = filterString(data.text, instruction.command)
-      console.log(tvObject)
-      var response = 'none'
+      var tvObject = this._mentionTvbot(data.text)
       var reqUrl = "http://api.tvmaze.com/singlesearch/shows?q=" + tvObject.tvShow + "&embed=episodes";
-      console.log(reqUrl)
-
-      request(reqUrl, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-          var res = JSON.parse(body)
-          var res = 'the next episode is called *' + res._embedded.episodes[res._embedded.episodes.length - 1].name + '*';
-          console.log(res)
-          lol.postMessageToChannel('general', res);
-        } else {
-          lol.postMessageToChannel('general', 'Sorry, I could not find that tvshow')
-        }
-      })
-    }
   }
 })
 
-var filterString = function(message, command) {
-    var indexOfCommand = message.indexOf(command) + command.length;
-    var tvShow = message.slice(indexOfCommand + 1, message.length)
 
-    tvShow = tvShow.replace(/\s+/g, '-')
-    console.log(tvShow)
-
-    return {
-      tvShow,
-      command
-    }
-
-}
 
 
 
